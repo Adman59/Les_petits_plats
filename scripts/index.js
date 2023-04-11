43e95da4c37caddb35209042e768e66b9ca3c6f4
amagnies <adrien.magnies@hotmail.fr>
2023-04-11 11:43:58 +0200

    Ajout de la fonctionnalité de la barre de recherche principale

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,23 @@ const recipesDomElements = recipes.map((recipe) => {
 
 listOfReceipts.innerHTML = recipesDomElements.join("");
 
+//----------------------------------------------------------------
+// Fonctionnalité de la barre de recherche principale avec Filter :
 
+const searchInput = document.querySelector("#searchbar__form__input");
 
+searchInput.addEventListener("input", filterList);
 
+function filterList() {
+    const filter = searchInput.value.toLowerCase();
+    const allRecipeCard = document.querySelectorAll(".card");
+
+    allRecipeCard.forEach((card) => {
+        let text = card.textContent;
+        if (text.toLowerCase().includes(filter.toLowerCase())) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    })
+}
